916f92048d9102927a726dbf40dc253e6e3078a5
George Silvis, III <george.iii.silvis@gmail.com>
2013-11-10 22:15:51 +0000

    Replace 'active' variable with dual 'waiting'

--- monitor.c
+++ monitor.c
@@ -8,7 +8,7 @@
 struct monitor {
   pthread_mutex_t lock;
   pthread_cond_t queue;
-  int active;
+  int waiting;
 };
 
 struct monitor_cond {
@@ -21,7 +21,7 @@ struct monitor *monitor_create (void) {
   struct monitor *returnee = (struct monitor *) malloc(sizeof(struct monitor));
   pthread_mutex_init(&returnee->lock, NULL);
   pthread_cond_init(&returnee->queue, NULL);
-  returnee->active = 0;
+  returnee->waiting = 0;
   return returnee;
 }
 
@@ -49,44 +49,40 @@ void monitor_run_fn (struct monitor *m,
                      void *user_data) {
   /* Grab lock, and enter monitor */
   pthread_mutex_lock(&m->lock);
-  m->active++;
-  if (m->active > 1) {
+  if (m->waiting) {
     /* There's another thread that's trying to execute now; defer to it to
        avoid starvation. */
+    m->waiting++;
     pthread_cond_wait(&m->queue, &m->lock);
+    m->waiting--;
   }
   /* Else, no threads trying to execute now; skip the wait to avoid
      deadlock */
   f(user_data);
   /* Leave monitor, and signal another waiting thread to execute */
-  m->active--;
   pthread_cond_signal(&m->queue);
   pthread_mutex_unlock(&m->lock);
 }
 
 /* Caller must own monitor mutex */
 void monitor_cond_wait (struct monitor_cond *c) {
-  /* Shift thread from 'active' to 'waiting on c' */
-  c->monitor->active--;
-  c->waiting++;
-  /* Queue up another thread, and wait */
+  /* Queue up another thread */
   pthread_cond_signal(&c->monitor->queue);
+  /* Wait */
+  c->waiting++;
   pthread_cond_wait(&c->cond, &c->monitor->lock);
-  /* Shift thread from 'waiting on c' to 'active' */
   c->waiting--;
-  c->monitor->active++;
 }
 
 /* Caller must own monitor mutex */
 void monitor_cond_signal (struct monitor_cond *c) {
   if (c->waiting) {
-    /* Shift thread from 'active' to 'inactive' */
-    c->monitor->active--;
-    /* Queue up another thread, and wait */
+    /* Queue up another thread */
     pthread_cond_signal(&c->cond);
+    /* Wait */
+    c->monitor->waiting++;
     pthread_cond_wait(&c->monitor->queue, &c->monitor->lock);
-    /* Shift thread from 'inactive' to 'active */
-    c->monitor->active++;
+    c->monitor->waiting--;
   }
   /* If no threads waiting on c, do nothing at all. */
 }
