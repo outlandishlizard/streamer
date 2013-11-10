4c5580579e8bb0340c66a3a915c77e85f33ccf67
George Silvis, III <george.iii.silvis@gmail.com>
2013-11-10 22:32:56 +0000

    Document API for monitor functionality

--- monitor.h
+++ monitor.h
@@ -4,6 +4,24 @@
 struct monitor;
 struct monitor_cond;
 
+/*
+ * This is an implementation of Hoare-style blocking monitors.
+ *
+ * The workflow is as follows: Create a new monitor with
+ * 'monitor_create'. Create any needed condition variables associated with the
+ * monitor with 'monitor_cond_create'. At this point, threads may call
+ * 'monitor_run_fn' on a work function.  At some point when no threads are in
+ * the monitor, you may destroy the monitor and condition variables with
+ * 'monitor_destroy' and 'monitor_cond_destroy'.
+ *
+ * Work functions should have these properties: They should not attempt to
+ * call 'monitor_run_fn' themselves.  They may wait on and signal conditions
+ * associated with the monitor, with 'monitor_cond_wait' and
+ * 'monitor_cond_signal'.
+ *
+ * It is unsafe to manipulate condition variables from outside the monitor.
+ */
+
 struct monitor *monitor_create (void);
 struct monitor_cond *monitor_cond_create (struct monitor *monitor);
 void monitor_destroy (struct monitor *m);
