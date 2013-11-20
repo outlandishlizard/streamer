268fd2a96b10636d47e544d267a2f1c2ad889043
George Silvis, III <george.iii.silvis@gmail.com>
2013-11-20 19:52:51 +0000

    Add options to global_config.h

--- global_config.h
+++ global_config.h
@@ -1,9 +1,19 @@
 #ifndef __GLOBAL_CONFIG_H
 #define __GLOBAL_CONFIG_H
 
-/* Commented:  use system's pthreads */
+/* Commented:   use system's pthreads */
 /* Uncommented: use mythreads */
 
 // #define USE_MYTHREADS
+
+/* Commented:   use bare pthreads calls */
+/* Uncommented: use monitor abstraction */
+
+// #define USE_MONITOR
+
+/* Commented:   stream text */
+/* Uncommented: stream video */
+
+// #define STREAM_VIDEO
 
 #endif
